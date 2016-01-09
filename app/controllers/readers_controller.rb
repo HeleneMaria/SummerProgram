113ea0fe94dd095dddf619ee7ef6cb0b668b5e9e
HeleneMaria <Ja10CdC.github>
2016-01-09 13:33:55 -0600

    Fix upper-case column issue

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,13 +1,13 @@
 class ReadersController < ApplicationController
   before_filter :signed_in_user
-  
-  
+
+
   def destroy
     Reader.find(params[:id]).destroy
     flash[:success] = "Reader deleted !"
     redirect_to readers_path
   end
-  
+
   helper_method :sort_column, :sort_direction
   def index
     if params[:schoolFallName] or params[:schoolFallGrade] or params[:prize]
@@ -23,7 +23,7 @@ class ReadersController < ApplicationController
       @readers = @readers.paginate(:page => params[:page], :per_page => 10)
     end
   end
-  
+
  def edit
     @reader = Reader.find(params[:id])
     @schools = Reader.select(:schoolFallName).distinct
@@ -31,7 +31,7 @@ class ReadersController < ApplicationController
     @grade = ["None","Pre-School","Pre-Kindergarten","Kindergarten","1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th"]
     @size = ["None","Youth Small","Youth Medium","Youth Large","Adult Small","Adult Medium","Adult Large","Adult X-Large","Adult 2 X-Large","Adult 3 X-Large"]
    end
-  
+
   def update
     @reader = Reader.find(params[:id])
     if @reader.update_attributes(reader_param)
@@ -46,21 +46,21 @@ class ReadersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
     @reader = Reader.find(params[:id])
     @book = @reader.books.build
     id = Prize.select(:id).where(reader_id: params[:id]).to_a #a simple where sends back a activerecord:relation instead of an activerecord
     @prize = Prize.find(id.first)
   end
-  
+
   def create
     @reader = Reader.new(reader_param)
     if @reader.newSchoolFallName != ""
         puts "if @reader.newSchoolFallName"
       @reader.schoolFallName =@reader.newSchoolFallName
       @reader.newSchoolFallName=nil
-      
+
     end
     if @reader.save
       @prize = Prize.new(level:0,reader_id:@reader.id)
@@ -71,7 +71,7 @@ class ReadersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def new
    @reader = Reader.new
    @schools = Reader.select(:schoolFallName).distinct
@@ -79,25 +79,25 @@ class ReadersController < ApplicationController
        @grade = ["None","Pre-School","Pre-Kindergarten","Kindergarten","1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th"]
     @size = ["None","Youth Small","Youth Medium","Youth Large","Adult Small","Adult Medium","Adult Large","Adult X-Large","Adult 2 X-Large","Adult 3 X-Large"]
   end
-  
+
   def reader_param
     params.require(:reader).permit(:firstName, :lastName, :phoneNumber, :email, :schoolFallName, :schoolFallGrade, :program, :age, :tShirtSize, :newSchoolFallName, :books, :prize, :notes)
   end
-  
-  
+
+
   private
   def signed_in_user
     redirect_to signin_path, notice: "Please sign in." unless signed_in?
   end
-  
+
   private
   def sort_column
-    Reader.column_names.include?(params[:sort]) ? params[:sort] : "firstName"
+    Reader.column_names.include?(params[:sort]) ? params[:sort] : "\"lastName\""
   end
-  
+
   private
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
-  
+
 end
