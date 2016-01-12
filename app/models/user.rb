29ef0825110b2f89e4c35ee4b24736855bc899c5
HeleneMaria <Ja10CdC.github>
2016-01-11 18:09:31 -0600

    admin signed in dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,16 @@
 
 
 class User < ActiveRecord::Base
-  
+
   has_secure_password
-  
+
   validates :username, presence:true, uniqueness:{ case_sensitive: false}
   validates :password, presence:true,length: {minimum: 6}
-  
-  before_save :create_remember_token
-  
+
+  before_save :create_remember_token #for the session, being able to find it on everypage once signed in
+
   #all methods under private are automatically hidden (no possibility to see it in the console for example)
-  private 
+  private
   def create_remember_token #for the sign_in to be persistent even after the browser is closed
     self.remember_token = SecureRandom.urlsafe_base64
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329150951) do
+ActiveRecord::Schema.define(version: 20160111234207) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150329150951) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
